a23113b4958b2ea57d7d737ade505a01b263f8c7
MuriloGGSilva <127908106+MuriloGGSilva@users.noreply.github.com>
2023-08-30 21:47:53 -0300

    Create 2.10.ts

---
+++ unidade_2/2.10.ts
@@ -0,0 +1,22 @@
+class Fibonacci {
+    private memo: Map<number, number> = new Map();
+
+    calcularTermo(n: number): number {
+        if (this.memo.has(n)) {
+            return this.memo.get(n)!;
+        }
+
+        if (n <= 2) {
+            return 1;
+        }
+
+        const termo = this.calcularTermo(n - 1) + this.calcularTermo(n - 2);
+        this.memo.set(n, termo);
+        return termo;
+    }
+}
+
+const fibonacciCalculator = new Fibonacci();
+
+console.log(fibonacciCalculator.calcularTermo(6)); // Exemplo de cálculo do termo 6
+console.log(fibonacciCalculator.calcularTermo(8)); // Exemplo de cálculo do termo 8
