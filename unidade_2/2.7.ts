be837fa7bcbe9dbcfa1d8b7272241c4cbb284527
MuriloGGSilva <127908106+MuriloGGSilva@users.noreply.github.com>
2023-08-30 21:47:12 -0300

    Create 2.7.ts

---
+++ unidade_2/2.7.ts
@@ -0,0 +1,58 @@
+class Veiculo {
+    protected latitude: number;
+    protected longitude: number;
+
+    constructor(latitude: number, longitude: number) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    mover(latitude: number, longitude: number): void {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        console.log(`Veículo movido para (${this.latitude}, ${this.longitude})`);
+    }
+}
+
+class VeiculoEletrico extends Veiculo {
+    private bateria: number;
+
+    constructor(latitude: number, longitude: number, bateria: number) {
+        super(latitude, longitude);
+        this.bateria = bateria;
+    }
+
+    carregarBateria(novaCarga: number): void {
+        this.bateria += novaCarga;
+        console.log(`Bateria carregada para ${this.bateria}%`);
+    }
+}
+
+class VeiculoEletricoVoador extends VeiculoEletrico {
+    private altitude: number;
+
+    constructor(latitude: number, longitude: number, bateria: number, altitude: number) {
+        super(latitude, longitude, bateria);
+        this.altitude = altitude;
+    }
+
+    mover(latitude: number, longitude: number): void {
+        super.mover(latitude, longitude);
+        console.log(`Veículo voador movido para (${this.latitude}, ${this.longitude}, ${this.altitude})`);
+    }
+
+    moverVoador(latitude: number, longitude: number, altitude: number): void {
+        super.mover(latitude, longitude);
+        this.altitude = altitude;
+        console.log(`Veículo voador movido para (${this.latitude}, ${this.longitude}, ${this.altitude})`);
+    }
+}
+
+const meuVeiculo = new VeiculoEletricoVoador(0, 0, 80, 100);
+meuVeiculo.mover(10, 15);
+meuVeiculo.carregarBateria(20);
+meuVeiculo.moverVoador(20, 30, 500);
+
+
+//tsc 2.7.ts
+//node 2.7.js
