a020cec3a4ecfed572ebd92d5813f96ed575ddbb
MuriloGGSilva <127908106+MuriloGGSilva@users.noreply.github.com>
2023-08-30 21:46:30 -0300

    Create 2.6a.ts

---
+++ unidade_2/2.6a.ts
@@ -0,0 +1,21 @@
+enum MesesDoAno {
+    JANEIRO = "Janeiro",
+    FEVEREIRO = "Fevereiro",
+    MARCO = "Março",
+    ABRIL = "Abril",
+    MAIO = "Maio",
+    JUNHO = "Junho",
+    JULHO = "Julho",
+    AGOSTO = "Agosto",
+    SETEMBRO = "Setembro",
+    OUTUBRO = "Outubro",
+    NOVEMBRO = "Novembro",
+    DEZEMBRO = "Dezembro"
+}
+
+// Exemplo de uso da enumeração
+const mesAtual: MesesDoAno = MesesDoAno.AGOSTO;
+
+console.log("O mês atual é:", mesAtual);
+
+
