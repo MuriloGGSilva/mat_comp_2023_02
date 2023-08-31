e1900b2934f9db3fffc1f17d466eac37e632b070
MuriloGGSilva <127908106+MuriloGGSilva@users.noreply.github.com>
2023-08-30 21:47:36 -0300

    Create 2.9.ts

---
+++ unidade_2/2.9.ts
@@ -0,0 +1,20 @@
+class ProgressaoGeometrica {
+    constructor(private primeiroTermo: number, private razao: number) {}
+
+    gerarTermos(n: number): number[] {
+        const termos: number[] = [this.primeiroTermo];
+        for (let i = 1; i < n; i++) {
+            const proximoTermo = termos[i - 1] * this.razao;
+            termos.push(proximoTermo);
+        }
+        return termos;
+    }
+}
+
+// Criando uma instância da Progressão Geométrica com primeiro termo 2 e razão 3
+const pg = new ProgressaoGeometrica(2, 3);
+
+// Gerando os primeiros 50 termos da progressão geométrica
+const primeiros50Termos = pg.gerarTermos(50);
+
+console.log(primeiros50Termos);
