e19bfc2d87f2bc0ca4ae47e396247c82cf6753c9
MuriloGGSilva <127908106+MuriloGGSilva@users.noreply.github.com>
2023-08-30 21:46:40 -0300

    Create 2.6b.ts

---
+++ unidade_2/2.6b.ts
@@ -0,0 +1,14 @@
+enum DiasDaSemana {
+    DOMINGO = "Domingo",
+    SEGUNDA = "Segunda-feira",
+    TERCA = "Terça-feira",
+    QUARTA = "Quarta-feira",
+    QUINTA = "Quinta-feira",
+    SEXTA = "Sexta-feira",
+    SABADO = "Sábado"
+}
+
+// Exemplo de uso da enumeração
+const diaAtual: DiasDaSemana = DiasDaSemana.QUARTA;
+
+console.log("O dia atual é:", diaAtual);
